097dc8df3cc016389420438d98a35082fb1fa255
dauntless-strategy-1036 <danielgordon19972@gmail.com>
2020-11-05 12:48:48 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -14,7 +14,7 @@ function accessElementInArray(array, index) {
   return array[index]
 }
 function addElementToEndOfArray(array, element) {
-  return [array, ...element]
+  return [...array, element]
 }
 function destructivelyAddElementToEndOfArray(array, element) {
   array.push(element)
