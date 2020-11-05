ab6513a64653bae25d8e1ab1d64d452fb6440047
dauntless-strategy-1036 <danielgordon19972@gmail.com>
2020-11-05 12:39:45 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -2,14 +2,14 @@ var chocolateBars = ["snickers", "hundred grand", "kitkat", "skittles"];
 
 var newArray = [];
 
-var function addElementToBeginningOfArray(array, element) {
+function addElementToBeginningOfArray(array, element) {
   return [element, ...array];
 }
 
-var function destructivelyAddElementToBeginningOfArray(array, element) {
+function destructivelyAddElementToBeginningOfArray(array, element) {
   array.unshift(element);
   return array
 }
-var function accessElementInArray() {
-  
+function accessElementInArray(array, index) {
+  return array[index]
 }
